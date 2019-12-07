474e0fb52550d7a3742797afafe0761cd7ff3234
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2019-12-07 11:41:21 +0100

    Primer commit de integración Eclipse - Java

---
+++ OracleILearningUsers/src/Principal.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jorge Dueñas Lerín
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo git");
+	}
+
+}
