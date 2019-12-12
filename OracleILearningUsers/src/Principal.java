b68fd996bb7b5fbf930680b352add2f18de18636
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2019-12-12 16:51:39 +0100

    Versión temporal con XSLT 2.0 de prueba
    Se ha incluido la librería Saxon que soporta XSLT2.0
    Se ejcuta y transforma un ejemplo de prueba

--- OracleILearningUsers/src/Principal.java
+++ OracleILearningUsers/src/Principal.java
@@ -1,11 +1,27 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.xml.transform.stream.StreamSource;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 /**
  * 
  */
+import org.apache.commons.cli.ParseException;
+
+import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.Serializer;
+import net.sf.saxon.s9api.Xslt30Transformer;
+import net.sf.saxon.s9api.XsltCompiler;
+import net.sf.saxon.s9api.XsltExecutable;
 
 /**
  * @author Jorge Dueñas Lerín
@@ -13,11 +29,39 @@ import java.util.ArrayList;
  */
 public class Principal {
 
+	public static void transformData(String stylesheetPath, String csvDataPath) throws SaxonApiException{
+		Processor processor = new Processor(false);
+        XsltCompiler compiler = processor.newXsltCompiler();
+        XsltExecutable stylesheet = compiler.compile(new StreamSource(new File("styles/books.xsl")));
+        Serializer out = processor.newSerializer(new File("books.html"));
+        out.setOutputProperty(Serializer.Property.METHOD, "html");
+        out.setOutputProperty(Serializer.Property.INDENT, "yes");
+        Xslt30Transformer trans = stylesheet.load30();
+        trans.transform(new StreamSource(new File("data/books.xml")), out);
+
+        System.out.println("Output written to books.html");
+	}
+	
 	/**
 	 * @param args
 	 * @throws IOException 
+	 * @throws ParseException 
+	 * @throws SaxonApiException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws ParseException, SaxonApiException{
+		
+		final Options options = new Options();
+		options.addOption(new Option("d", "debug", false, "Turn on debug."));
+		options.addOption(new Option("e", "extract", false, "Turn on extract."));
+		options.addOption(new Option("o", "option", true, "Turn on option with argument."));
+		
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse( options, args);
+		
+		transformData("data/plantilla.xml", "");
+		
+		
+		/*
 		String row;
 		
 		ArrayList<Persona> listado = new ArrayList<Persona>();
@@ -39,6 +83,7 @@ public class Principal {
 		csvReader.close();
 		
 		System.out.println(General.getFileContent(listado));
+		*/
 	}
 
 }
