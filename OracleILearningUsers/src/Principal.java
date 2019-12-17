80170708697f94b6387eda60d0f0edcd3e83a904
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2019-12-17 10:01:43 +0100

    Primera versión
    Acepta los parámetros de la línea de comandos necesarios para gestionar
    varios ficheros.
    
    También se parametriza si queremos acceso total (profesores) o solo
    certificaciones Java y DB

--- OracleILearningUsers/src/Principal.java
+++ OracleILearningUsers/src/Principal.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +8,6 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 /**
@@ -25,7 +20,6 @@ import net.sf.saxon.s9api.QName;
 import net.sf.saxon.s9api.SaxonApiException;
 import net.sf.saxon.s9api.Serializer;
 import net.sf.saxon.s9api.XdmAtomicValue;
-import net.sf.saxon.s9api.XdmItem;
 import net.sf.saxon.s9api.XdmValue;
 import net.sf.saxon.s9api.Xslt30Transformer;
 import net.sf.saxon.s9api.XsltCompiler;
@@ -52,13 +46,14 @@ public class Principal {
 			String csvDataPath,
 			String anio,
 			String df_pass,
-			String tutor
+			String tutor,
+			String extendido
 			) throws SaxonApiException {
 		
         Processor processor = new Processor(false);
         XsltCompiler compiler = processor.newXsltCompiler();
         XsltExecutable exp = compiler.compile(new StreamSource(
-        		Principal.class.getResourceAsStream(TEMPLATE)
+        							Principal.class.getResourceAsStream(TEMPLATE)
         									)
         								);
         
@@ -72,6 +67,10 @@ public class Principal {
         parameters.put(new QName("anio"), new XdmAtomicValue(anio));
         parameters.put(new QName("df_pass"), new XdmAtomicValue(df_pass));
         parameters.put(new QName("tutor"), new XdmAtomicValue(tutor));
+        parameters.put(new QName("extendido"), new XdmAtomicValue(extendido));
+        
+        parameters.put(new QName("csv-uri"), new XdmAtomicValue(csvDataPath));
+        
         
 		trans.setInitialTemplateParameters(parameters, false);
 		
@@ -90,29 +89,78 @@ public class Principal {
 		final Options options = new Options();
 		
 		options.addOption(
-					Option.builder("d")
-						.longOpt("datos")
-						.argName("datos")
-						.required()
-						.build()
-				);
-				
+				Option.builder("d")
+					.longOpt("datos")
+					.argName("datos")
+					.hasArg()
+					.desc("Ruta al fichero de datos")
+					.required()
+					.build()
+			);
+		
+		options.addOption(
+				Option.builder("y")
+					.longOpt("año")
+					.hasArg()
+					.argName("año")
+					.desc("Prefijo para gestionar varios años de usuarios")
+					.required()
+					.build()
+			);
+		
+		options.addOption(
+				Option.builder("p")
+					.longOpt("pass")
+					.hasArg()
+					.argName("pass")
+					.desc("Contraseña por defecto")
+					.required()
+					.build()
+			);
+		
+		// Para el JDLC: ies_juan_de_la_cierva
+		options.addOption(
+				Option.builder("t")
+					.longOpt("tutor")
+					.hasArg()
+					.argName("tutor")
+					.desc("Usuario tutor en plataforma\nPara el JDLC: ies_juan_de_la_cierva")
+					.required()
+					.build()
+			);
+		
+		options.addOption(
+				Option.builder("e")
+					.longOpt("extendido")
+					.hasArg()
+					.argName("extendido")
+					.desc("Todos los cursos! y profesor!!")
+					.build()
+			);
 		
 		CommandLineParser parser = new DefaultParser();
 		
 		try {
 			CommandLine cmd = parser.parse( options, args);
+			String file = cmd.getOptionValue("d");
+			String año = cmd.getOptionValue("y");
+			String pass = cmd.getOptionValue("p");
+			String tutor = cmd.getOptionValue("t");
+			String extendido = cmd.getOptionValue("e");
+			
 			transformData(
-					"data/ejemplo.csv",
-					"19_20",
-					"jdlccambiala",
-					"jorge_duenas_lerin"
+					file,
+					año,
+					pass,
+					tutor,
+					extendido
 				);
+			
 		} catch (ParseException pe) {
 			System.out.println("Error en parámetros");
 			System.out.println(pe.getMessage());
 			HelpFormatter formatter = new HelpFormatter();
-			formatter.printHelp( "opciones", options );
+			formatter.printHelp( " ", options );
 		}
 	}
 
